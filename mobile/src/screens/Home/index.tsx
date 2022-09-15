3683b21a0fea9198dadb2ede0610a713f698c94f
Paulo Pires <prpires66@gmail.com>
2022-09-15 18:16:44 -0300

    Stage 2 - Final

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import {GAMES} from '../../utils/games';
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
@@ -11,19 +11,27 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image
-      source={logoImg}
-      style={styles.logo}
-      
+        source={logoImg}
+        style={styles.logo}
+
       />
       <Heading
-       title="Encontre seu duo!"
-       subtitle="Selecione o game que deseja jogar..."
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
 
-      <GameCard
-        data={GAMES[0]}
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          < GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.constentList}
       />
-
     </View>
   );
 }
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -10,5 +10,9 @@ export const styles = StyleSheet.create({
     height: 120,
     marginTop: 74,
     marginBottom: 48,
+  },
+  constentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
   }
 });
