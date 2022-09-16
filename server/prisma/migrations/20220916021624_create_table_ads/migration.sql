e54509d679bc773aa87ce538dccf3f7c5e377800
Paulo Pires <prpires66@gmail.com>
2022-09-15 23:20:03 -0300

    Stage 3 - 1:01:40

---
+++ server/prisma/migrations/20220916021624_create_table_ads/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Ad" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "gameId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" INTEGER NOT NULL,
+    "hoursStart" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,31 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response)=> {
+app.get('/games', (request, response) => {
+   return response.json([]);
+}) 
+
+app.post('/ads', (request, response) => {
+   return response.status(201).json([]);
+}) 
+
+app.get('/games/:id/ads', (request, response)=> {
+   const gameId = request.params.id;
+   return response.send(gameId);
+
    return response.json([
     { id: 1, name: 'Anuncio 1'},
     { id: 2, name: 'Anuncio 2'},
-    { id: 3, name: 'Anuncio 3'}
+    { id: 3, name: 'Anuncio 3'},
+   ]) 
+})
+
+app.get('/ads/:id/discord', (request, response)=> {
+   const gameId = request.params.id;
+   return response.send(gameId);
+
+   return response.json([
    ]) 
 })
 
